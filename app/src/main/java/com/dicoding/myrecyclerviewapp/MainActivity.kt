cb6faa852be1aee3330db8bd0e37d67fb124ffb9
Tricodeit <bayu165kusuma@gmail.com>
2021-04-20 14:37:19 +0700

    Mode Grid

--- app/src/main/java/com/dicoding/myrecyclerviewapp/MainActivity.kt
+++ app/src/main/java/com/dicoding/myrecyclerviewapp/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -33,23 +34,29 @@ class MainActivity : AppCompatActivity() {
         rvHeroes.adapter = listHeroAdapter
     }
 
+    private fun showRecyclerGrid() {
+        rvHeroes.layoutManager = GridLayoutManager(this, 2)
+        val gridHeroAdapter = GridHeroAdapter(list)
+        rvHeroes.adapter = gridHeroAdapter
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
+        setMode(item.itemId)
         return super.onOptionsItemSelected(item)
     }
 
     private fun setMode(selectedMode: Int) {
         when (selectedMode) {
             R.id.action_list -> {
-
+                showRecyclerList()
             }
             R.id.action_grid -> {
-
+               showRecyclerGrid()
             }
             R.id.action_cardview -> {
 
